115a52a48acc84bee8e80981197dce1e18aa271f
Akash Pal <creativeakashpal@gmail.com>
2023-12-02 20:30:14 +0530

    added login, logout Provider

--- blog-appwrite/src/App.jsx
+++ blog-appwrite/src/App.jsx
@@ -1,12 +1,32 @@
-
+import {useDispatch} from 'react-redux';
+import { useEffect, useState } from 'react'
 import './App.css'
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Header, Footer } from './components';
 
 function App() {
-  return (
-    <>
-      <h1>Blog app with appwrite</h1>
-    </>
-  )
+  const [ loading, setLoading ] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if(userData) {
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  ) : null;
 }
 
 export default App
