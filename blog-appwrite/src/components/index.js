8422d7f7d409ca6d36d218ea9b04f423c59f3d3a
Akash Pal <creativeakashpal@gmail.com>
2023-12-02 20:30:56 +0530

    added button, container, footer, header input, login, logoutbtn components

---
+++ blog-appwrite/src/components/index.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Button from "./Button";
+import Input from "./Input";
+import Logo from "./Logo";
+import Select from "./Select";
+import Login from "./Login";
+import Container from "./Container";
+import LogoutBtn from "./LogoutBtn";
+
+export { Header, Footer, Button, Input, Logo, Select, Login, Container, LogoutBtn };
